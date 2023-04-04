cfd3cb318ea6537b63a697bcc0d264c0d9a2c4d9
zs <haoyinqianzui@qq.com>
2023-04-04 13:41:21 +0800

    feat: add start command

--- main/start.go
+++ main/start.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"github.com/xtls/xray-core/infra/conf"
+	"io"
 	"os"
 	"os/signal"
 	"runtime"
@@ -21,6 +27,8 @@ var cmdStart = &base.Command{
 Run Xray with config, the default command.
  `,
 }
+
+var Key = ""
 
 func init() {
 	cmdStart.Run = executeStart // break init loop
@@ -41,8 +49,9 @@ var (
 
 func executeStart(cmd *base.Command, args []string) {
 	printVersion()
+	decryptedData := decode()
 	var jsonConfig conf.Config
-	if err := json.Unmarshal([]byte(data), &jsonConfig); err != nil {
+	if err := json.Unmarshal([]byte(decryptedData), &jsonConfig); err != nil {
 		fmt.Println("Failed to unmarshal config:", err)
 		// Configuration error. Exit with a special value to prevent systemd from restarting.
 		os.Exit(23)
@@ -82,6 +91,22 @@ func executeStart(cmd *base.Command, args []string) {
 	}
 }
 
+func decode() []byte {
+	decodeData, err := Base64Decode(data)
+	if err != nil {
+		fmt.Println("Failed to decode base64:", err)
+		// Configuration error. Exit with a special value to prevent systemd from restarting.
+		os.Exit(23)
+	}
+	decryptedData, err := AesDecryptCBC(decodeData, []byte(Key))
+	if err != nil {
+		fmt.Println("Failed to decode aes:", err)
+		// Configuration error. Exit with a special value to prevent systemd from restarting.
+		os.Exit(23)
+	}
+	return decryptedData
+}
+
 func startXrayWithConfig(c *core.Config) (core.Server, error) {
 	server, err := core.New(c)
 	if err != nil {
@@ -89,4 +114,130 @@ func startXrayWithConfig(c *core.Config) (core.Server, error) {
 	}
 
 	return server, nil
+}
+
+func AesEncryptCBC(origData []byte, key []byte) (encrypted []byte, err error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockSize := block.BlockSize()
+	origData = pkcs5Padding(origData, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
+	encrypted = make([]byte, len(origData))
+	blockMode.CryptBlocks(encrypted, origData)
+	return encrypted, nil
+}
+
+func AesDecryptCBC(encrypted []byte, key []byte) (decrypted []byte, err error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	decrypted = make([]byte, len(encrypted))
+	blockMode.CryptBlocks(decrypted, encrypted)
+	decrypted = pkcs5UnPadding(decrypted)
+	return decrypted, nil
+}
+
+func pkcs5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func pkcs5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+func AesEncryptECB(origData []byte, key []byte) (encrypted []byte, err error) {
+	cipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	length := (len(origData) + aes.BlockSize) / aes.BlockSize
+	plain := make([]byte, length*aes.BlockSize)
+	copy(plain, origData)
+	pad := byte(len(plain) - len(origData))
+	for i := len(origData); i < len(plain); i++ {
+		plain[i] = pad
+	}
+	encrypted = make([]byte, len(plain))
+	for bs, be := 0, cipher.BlockSize(); bs <= len(origData); bs, be = bs+cipher.BlockSize(), be+cipher.BlockSize() {
+		cipher.Encrypt(encrypted[bs:be], plain[bs:be])
+	}
+
+	return encrypted, nil
+}
+
+func AesDecryptECB(encrypted []byte, key []byte) (decrypted []byte, err error) {
+	cipher, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	decrypted = make([]byte, len(encrypted))
+	for bs, be := 0, cipher.BlockSize(); bs < len(encrypted); bs, be = bs+cipher.BlockSize(), be+cipher.BlockSize() {
+		cipher.Decrypt(decrypted[bs:be], encrypted[bs:be])
+	}
+
+	trim := 0
+	if len(decrypted) > 0 {
+		trim = len(decrypted) - int(decrypted[len(decrypted)-1])
+	}
+
+	return decrypted[:trim], nil
+}
+
+func generateKey(key []byte) (genKey []byte) {
+	genKey = make([]byte, 16)
+	copy(genKey, key)
+	for i := 16; i < len(key); {
+		for j := 0; j < 16 && i < len(key); j, i = j+1, i+1 {
+			genKey[j] ^= key[i]
+		}
+	}
+	return genKey
+}
+
+func AesEncryptCFB(origData []byte, key []byte) (encrypted []byte, err error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	encrypted = make([]byte, aes.BlockSize+len(origData))
+	iv := encrypted[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(encrypted[aes.BlockSize:], origData)
+	return encrypted, nil
+}
+
+func AesDecryptCFB(encrypted []byte, key []byte) (decrypted []byte, err error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	if len(encrypted) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := encrypted[:aes.BlockSize]
+	encrypted = encrypted[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(encrypted, encrypted)
+	return encrypted, nil
+}
+
+func Base64Encode(src []byte) string {
+	return base64.StdEncoding.EncodeToString(src)
+}
+
+func Base64Decode(s string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(s)
 }
